0c1e19f740f63b63a2bfc8a88753f37c6a3575bf
siddarthrk <siddarthreddyk@gmail.com>
2021-06-17 13:36:26 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  invite: 'NTBlMGQzMDAtY2E4ZS0xMWViLWJkNzEtZjFmMjgyZGY3NjVkOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==',
+  invite: 'OWQ0ZjQ2MmQtY2YwYi0xMWViLWFiNDEtYTM5ODRjNGRiMzJmOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==',
   password: 'SvnKg7zFwkLc9mh9PAAWt',
   screenshotsDir: './screenshots',
   runs: 80,
